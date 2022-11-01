726661b342ab3584d976ec7b0e88c3b9d6d27126
nobodyyyyyy <zhuyuntiannn@163.com>
2022-11-01 16:13:26 +0800

    数据处理 nc

--- new/Util/TimeUtil.py
+++ new/Util/TimeUtil.py
@@ -1,3 +1,66 @@
+import time
+
+
 class TimeUtil:
 
-    pass
+    @staticmethod
+    def format_month_or_day(input_):
+        """
+        将月或日前置 0
+        :return:
+        """
+        if isinstance(input_, str):
+            if len(input_) == 1:
+                return '0' + input_
+            else:
+                print('input_ {} is str'.format(input_))
+                return input_
+        elif isinstance(input_, int):
+            if input_ > 10:
+                return str(input_)
+            elif input_ > 0:
+                return '0' + str(input_)
+        print('cannot transform for input_ {}'.format(input_))
+        return input_
+
+
+    @staticmethod
+    def current_time_millis():
+        return round(time.time() * 1000)
+
+
+    @staticmethod
+    def time_millis_2_nc_timestamp(time_millis):
+        """
+        nc 文件中的时间是 hours since 1900-01-01 00:00:00.0
+        Unix 时间戳是从1970年1月1日（UTC/GMT的午夜）开始所经过的秒数
+        转换 Unix 时间戳（秒）至 nc 时间戳
+        :param time_millis: Unix 时间戳
+        :return: nc 时间戳
+        """
+        day_diff = 25567
+        sec_diff = day_diff * 24 * 3600
+        return int((time_millis + sec_diff) / 3600)
+
+
+    @staticmethod
+    def nc_timestamp_2_time_millis(nc_timestamp):
+        """
+        nc 时间戳转 Unix 时间戳（秒）
+        """
+        day_diff = 25567
+        sec_diff = day_diff * 24 * 3600
+        return nc_timestamp * 3600 - sec_diff
+
+
+    @staticmethod
+    def get_day_sum(year, month):
+        if month in (1, 3, 5, 7, 8, 10, 12):
+            return 31
+        elif month in (4, 6, 9, 11):
+            return 30
+        elif month == 2:
+            if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+                return 29
+            else:
+                return 28
--- new/data/DataUtil.py
+++ new/data/DataUtil.py
@@ -1,5 +1,11 @@
+import bisect
+import time
+
 import numpy as np
 import netCDF4 as nc
+from numpy import ndarray
+
+from new.Util.TimeUtil import TimeUtil
 
 
 class DataUtils:
@@ -43,41 +49,98 @@ class DataUtils:
 
 
     @staticmethod
-    def get_support_data(year, month, type_, lan, lon, time, level=-1):
+    def get_idx_for_val_pos(arr:ndarray, val, is_reverse=False, is_strict=False):
+        """
+        在 nc 文件中的 lan 或 lng 中找到 val 值对应的下标 idx
+        nc 文件的 time 也是类似的调用该方法
+        :param val:
+        :param arr:
+        :param is_reverse:
+        :param is_strict: 是否严格查找
+        """
+        n = len(arr)
+        if is_reverse:
+            reverse_arr = np.flip(arr)
+            idx = DataUtils.inner_get_idx(reverse_arr, val, is_strict)
+            return n - idx - 1
+        else:
+            return DataUtils.inner_get_idx(arr, val, is_strict)
+
+
+    @staticmethod
+    def inner_get_idx(arr:ndarray, val, is_strict=False):
+        n = len(arr)
+        idx = bisect.bisect(arr, val)
+        if arr[idx] == val:
+            return idx
+        if is_strict:
+            return -1
+        if idx >= n:
+            # 考虑 arr 足够长
+            return idx - 1
+        else:
+            # 取近邻
+            # 要知道如果没找到，bisect 默认取右值
+            if idx == 0:
+                return idx
+            elif arr[idx] - val > val - arr[idx - 1]:
+                return idx - 1
+            else:
+                return idx
+
+
+    @staticmethod
+    def get_support_data(year:int, month:int, type_:str, lan:float, lng:float, time_, level=-1):
         """
         获取辅助数据，主要是 nc 文件内容
         :param month:
         :param year:
         :param type_: omega, q, skt, slp, sst, temp, u10m, uwind, v10m, vwind, zg
         :param lan: 纬度
-        :param lon: 经度
-        :param time: 标准的秒级别时间戳，本方法会做对应的转换
+        :param lng: 经度
+        :param time_: 标准的【豪秒】级别时间戳
         :param level: air pressure level 只能取特定的取值，见文档
         :return: 所需 data
         """
+        month = TimeUtil.format_month_or_day(month)
         file = './AEM/{}.{}-{}.daily.nc'.format(type_, year, month)
         dataset = nc.Dataset(file)
-        all_vars = dataset.variables.keys()
-        all_vars_info = dataset.variables.items()
-
+        nc_timestamp = TimeUtil.time_millis_2_nc_timestamp(time_)
+        lats = np.array(dataset.variables['latitude'][:])
+        lngs = np.array(dataset.variables['longitude'][:])
+        times = np.array(dataset.variables['time'][:])
+        # find corresponding idx for lan, lng, time
+        lat_idx = DataUtils.get_idx_for_val_pos(lats, lan, is_reverse=True)
+        lng_idx = DataUtils.get_idx_for_val_pos(lngs, lng)
+        try:
+            time_idx = DataUtils.get_idx_for_val_pos(times, nc_timestamp)
+        except IndexError as e:
+            # q 数据集时间是有缺失的我真无语住了
+            # 这种情况就（合理地）认为，使用传入的时间戳 time 转换为临近的日作为 time_idx
+            print(e)
+            year = time.localtime(time_).tm_year
+            month = time.localtime(time_).tm_mon
+            day = time.localtime(time_).tm_mday
+            hour = time.localtime(time_).tm_hour
+            if hour >= 12 and day != 1 and day != TimeUtil.get_day_sum(year, month):
+                time_idx = day  # 加一天
+            else:
+                time_idx = day - 1
 
         if type_ in ['skt', 'slp', 'sst', 'u10m', 'v10m']:
             # data with shape (time, latitude, longitude)
-
-
-
-            pass
-        else:
-            pass
-
-        # lat = dataset.variables['latitude'][:]
-        # print(lat.shape)
-        # var_data = np.array(lat)
-        # print(var_data)
-
+            return dataset.variables[type_][time_idx][lat_idx][lng_idx]
+        elif type_ in ['omega', 'q', 'temp', 'uwind', 'vwind', 'zg']:
+            # data with shape (time, level, latitude, longitude)
+            if level == -1:
+                print('Request level')
+                return -1
+            levels = np.array(dataset.variables['level'][:])
+            level_idx = DataUtils.get_idx_for_val_pos(levels, level, is_strict=False)  # todo 大气压是否要严格相等呢？
+            return dataset.variables[type_][time_idx][level_idx][lat_idx][lng_idx]
 
 
 if __name__ == '__main__':
-    DataUtils.aem_data_to_npy('./AEM/AEM00041217-data.txt', './AEM/AEM00041217-data.npy')
-    read_dictionary = np.load('./AEM/AEM00041217-data.npy', allow_pickle=True)
-    pass
+    # DataUtils.aem_data_to_npy('./AEM/AEM00041217-data.txt', './AEM/AEM00041217-data.npy')
+    # read_dictionary = np.load('./AEM/AEM00041217-data.npy', allow_pickle=True)
+    DataUtils.get_support_data(2018, 1, 'omega', 42.2, 42.2, 1514977200, 150)
