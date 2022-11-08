762520dbeff79fb9c939fd58f20cd9580fbcccfe
nobodyyyyyy <zhuyuntiannn@163.com>
2022-11-08 15:50:34 +0800

    matlab 转写 babin 完成

--- new/Util/DuctHeightUtil.py
+++ new/Util/DuctHeightUtil.py
@@ -47,5 +47,29 @@ def psit_nps(zet):
     return psi
 
 
+def psiu_25(zet):
+
+    if zet.real <= 0:
+        psi = -5 * zet
+        return psi
+
+    x = (1 - 16 * zet) * .25
+    psik = 2 * log((1 + x) / 2)+ log((1 + x * x) / 2) - 2 * atan(x) + 2 * atan(1)
+    x= (1 - 12.87 * zet) * .3333
+    psic = 1.5 * log((1 + x + x * x) / 3) - sqrt(3) * atan((1 + 2 * x) / sqrt(3)) + 4 * atan(1) / sqrt(3);
+    f = zet * zet / (1 + zet * zet)
+    psi = (1 - f) * psik + f * psic
+
+    return psi
 
 
+def psit_25(zet):
+    return psit_nps(zet)
+
+
+def fait(zet):
+    x = (1 - 16 * zet)**0.5
+    y = (1 - 12.87 * zet)**0.3333
+    f = zet * zet / (1 + zet * zet)
+    psi = (1 - f) / x + f / y
+    return psi
